5eb58d30c330f3ba441746a82e8dd1740a3cd983
garshishka <garshishka@yandex.ru>
2022-09-09 18:14:36 +0300

    Making like button change on click Adding viewbinding

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,19 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        findViewById<ImageButton>(R.id.likesIcon).setOnClickListener{
+            if(it !is ImageButton){
+                return@setOnClickListener
+            }
+            it.setImageResource(R.drawable.ic_baseline_favorite_24)
+        }
     }
 }
