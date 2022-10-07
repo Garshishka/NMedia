ba23fa45daa95c9fe639466de54bc2893eb95d36
garshishka <garshiv@gmail.com>
2022-10-07 17:51:54 +0300

    Added file storage for posts

--- app/src/main/java/ru/netology/nmedia/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/PostViewModel.kt
@@ -1,20 +1,23 @@
 package ru.netology.nmedia
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryFileImpl
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+import ru.netology.nmedia.repository.PostRepositorySharedPrefImpl
 
 private val empty = Post(
     id = 0,
     content = "",
     author = "",
     published = "",
-
 )
 
-class PostViewModel : ViewModel() {
-    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: PostRepository = PostRepositoryFileImpl(application)
     val data = repository.getAll()
     val edited = MutableLiveData(empty)
 
