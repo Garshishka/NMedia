6c99c985c7e41b43c341673b411f6733f92f829b
garshishka <garshiv@gmail.com>
2022-10-10 18:02:58 +0300

    Single activity with fragments

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -1,20 +1,19 @@
 package ru.netology.nmedia.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
 import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.R
-import ru.netology.nmedia.databinding.ActivityIntentHandlerBinding
-import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.activity.NewPostFragment.Companion.textArg
+import ru.netology.nmedia.databinding.ActivityAppBinding
 
-class IntentHandlerActivity : AppCompatActivity() {
-    lateinit var binding: ActivityIntentHandlerBinding
-
+class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityIntentHandlerBinding.inflate(layoutInflater)
+        val binding = ActivityAppBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         intent?.let {
@@ -31,7 +30,13 @@ class IntentHandlerActivity : AppCompatActivity() {
                     .show()
                 return@let
             }
-            // TODO: handle text
+            //findNavController(R.id.container).navigate(R.id.action_feedFragment_to_newPostFragment,
+            val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+            navHostFragment.navController.navigate(
+                R.id.action_feedFragment_to_newPostFragment,
+                Bundle().apply { textArg = text }
+            )
         }
     }
 }
