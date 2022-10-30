d09f2c9f3d77390dc79c027dc3c08b722e44a30f
garshishka <garshiv@gmail.com>
2022-10-30 19:30:02 +0300

    Can receive push notifications

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -2,8 +2,11 @@ package ru.netology.nmedia.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
 import com.google.android.material.snackbar.Snackbar
 import ru.netology.nmedia.R
@@ -37,5 +40,26 @@ class AppActivity : AppCompatActivity() {
                 Bundle().apply { textArg = text }
             )
         }
+
+        checkGoogleApiAvailability()
+    }
+
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@AppActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)) {
+                getErrorDialog(this@AppActivity, code, 9000)?.show()
+                return
+            }
+            Toast.makeText(this@AppActivity, R.string.google_play_unavailable, Toast.LENGTH_LONG)
+                .show()
+        }
+
+        /*FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            println(it)
+        }*/
     }
 }
