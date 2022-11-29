1f9cc4c6c53f57877bd9931e31d6c2f2e8616586
garshishka <garshiv@gmail.com>
2022-11-30 01:35:39 +0300

    Added asynchronous liking, removing and saving(editing) posts

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -9,9 +9,7 @@ import ru.netology.nmedia.Post
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
-class PostRepositoryImpl(
-    //private val dao: PostDao
-) : PostRepository {
+class PostRepositoryImpl() : PostRepository {
     private val client = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .build()
@@ -72,11 +70,44 @@ class PostRepositoryImpl(
             } ?: error("Empty response body")
     }
 
-    override fun likeById(id: Long): Post {
+    override fun saveAsync(post: Post, callback: PostRepository.SaveCallback) {
         val request: Request = Request.Builder()
-            .post(gson.toJson(id).toRequestBody(jsonType))
-            .url("${BASE_URL}/api/slow/posts/$id/likes")
+            .url("${BASE_URL}/api/slow/posts")
+            .post(gson.toJson(post).toRequestBody(jsonType))
             .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callback.onError(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                try {
+                    callback.onSuccess(gson.fromJson(body, Post::class.java))
+                } catch (e: Exception) {
+                    callback.onError(e)
+                }
+            }
+        })
+    }
+
+    private fun getLikeReq(id: Long, willLike: Boolean): Request {
+        return if (willLike) {
+            Request.Builder()
+                .post(gson.toJson(id).toRequestBody(jsonType))
+                .url("${BASE_URL}/api/slow/posts/$id/likes")
+                .build()
+        } else {
+            Request.Builder()
+                .delete(gson.toJson(id).toRequestBody(jsonType))
+                .url("${BASE_URL}/api/slow/posts/$id/likes")
+                .build()
+        }
+    }
+
+    override fun likeById(id: Long, willLike: Boolean): Post {
+        val request = getLikeReq(id, willLike)
 
         return client.newCall(request)
             .execute().let {
@@ -86,18 +117,24 @@ class PostRepositoryImpl(
             } ?: error("Empty response body")
     }
 
-    override fun unLikeById(id: Long): Post {
-        val request: Request = Request.Builder()
-            .delete(gson.toJson(id).toRequestBody(jsonType))
-            .url("${BASE_URL}/api/slow/posts/$id/likes")
-            .build()
+    override fun likeByIdAsync(id: Long, willLike: Boolean, callback: PostRepository.LikeCallback) {
+        val request = getLikeReq(id, willLike)
 
-        return client.newCall(request)
-            .execute().let {
-                it.body?.string()
-            }?.let {
-                gson.fromJson(it, Post::class.java)
-            } ?: error("Empty response body")
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callback.onError(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                try {
+                    callback.onSuccess(gson.fromJson(body, Post::class.java))
+                } catch (e: Exception) {
+                    callback.onError(e)
+                }
+            }
+
+        })
     }
 
     override fun shareById(id: Long) {
@@ -111,5 +148,22 @@ class PostRepositoryImpl(
             .build()
 
         client.newCall(request).execute()
+    }
+
+    override fun removeByIdAsync(id: Long, callback: PostRepository.RemoveCallback) {
+        val request: Request = Request.Builder()
+            .url("${BASE_URL}/api/slow/posts/$id")
+            .delete()
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callback.onError(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                callback.onSuccess()
+            }
+        })
     }
 }
