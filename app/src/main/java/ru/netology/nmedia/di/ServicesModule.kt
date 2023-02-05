a647c2ae920c520b4b3da2460b64bf83b56f9a7e
garshishka <garshiv@gmail.com>
2023-02-05 17:54:56 +0300

    simplified Module for services

--- app/src/main/java/ru/netology/nmedia/di/ServicesModule.kt
+++ app/src/main/java/ru/netology/nmedia/di/ServicesModule.kt
@@ -6,17 +6,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 class ServicesModule {
 
-    @Singleton
     @Provides
     fun provideFirebaseMessaging():FirebaseMessaging = FirebaseMessaging.getInstance()
 
-    @Singleton
     @Provides
     fun provideGoogleApiAvailability(): GoogleApiAvailability = GoogleApiAvailability.getInstance()
 }
